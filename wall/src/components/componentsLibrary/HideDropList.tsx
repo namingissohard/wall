4fdd2256ac63e8eea63e8b331848bcc32bf26b63
chico.zeng <chico.zeng@starlight.com>
2018-12-07 17:59:34 +0800

    漏了个文件emmm

--- wall/src/components/componentsLibrary/HideDropList.tsx
+++ wall/src/components/componentsLibrary/HideDropList.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
-interface ElementCallback{
+interface ElementCallback {
     element: HTMLDivElement,
     callbacks: Function[]
 }
-const elementCallbackStack:ElementCallback[] =[];
+const elementCallbackStack: ElementCallback[] = [];
 
+const _onClickDocument = (e: Event) => {
+    elementCallbackStack.map(elementCallback => {
+        if (hideDropList.isParent(e.target, elementCallback.element)||e.target === elementCallback.element) {
+           return 
+        }
+        elementCallback.callbacks.map(callback=>callback())
+    })
+}
+window.document.body.addEventListener('click', _onClickDocument)
 
-window.document.body.addEventListener('click',_onClickDocument)
+const hideDropList = {
+    init: (element: HTMLDivElement, callback: Function)=>{
+        const currentElement =  elementCallbackStack.filter(elementCallback=>elementCallback.element=== element)
+        if(currentElement.length>0){
+            currentElement[0].callbacks.push(callback)
+        }
+        else{
+            elementCallbackStack.push({element: element, callbacks: [callback]})
+        }
+    },
+    isParent:(child, parent: ElementCallback)=>{
+        if (parent && parent.callbacks.length && e.target === elementCallback.element) {
+            return true
+        }
+    }
+
+}
